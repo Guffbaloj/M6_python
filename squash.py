0dcc1045757bad6369128231b364942d16e9b8c8
Elliot Harrysson <me@hydrolox.me>
2025-10-20 10:36:26 +0200

    lägg till asynkron input

--- squash.py
+++ squash.py
@@ -1,8 +1,22 @@
+import sys
+import threading
+
+input_char = None
+
+def background_input():
+    while True:
+        global input_char
+        input_char = input("")
+
+background_input_thread = threading.Thread(target=background_input)
+background_input_thread.start()
+
 players = [[0,4],[29,4]]
 ball = [15,4]
 
 def drawScreen(positionsList, ballPos):
     print(f"\n"*10)
+    print(input_char)
     for y in range(10):
         row = ""
         for x in range(30):
