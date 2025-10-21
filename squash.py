a6ab21f0e16d8dea8e7e4c086903529fa272edc3
Elliot Harrysson <me@hydrolox.me>
2025-10-21 13:53:12 +0200

    pygame-fönster

--- squash.py
+++ squash.py
@@ -2,6 +2,7 @@ import sys
 import threading
 import math
 import time
+import pygame
 
 input_char = None
 
@@ -13,10 +14,6 @@ def background_input():
     while True:
         global input_char
         input_char = input("")
-
-
-
-
 
 def drawScreen(positionsList, ballPos):
     print(f"\n"*10)
@@ -39,7 +36,7 @@ def updateBall(ball):
     ballSpeed = ball[2]
 
     for _ in range(ballSpeed):
-        BOUNCE_ANGLE = math.pi + math.pi/4
+        BOUNCE_ANGLE = math.pi + 0.05
         if ballX < 0:
             ballDir += BOUNCE_ANGLE
         if ballY < 0:
@@ -62,8 +59,40 @@ def updateBall(ball):
 background_input_thread = threading.Thread(target=background_input)
 background_input_thread.start()
 
-while True:
-    drawScreen(players,ball[0])
-    updateBall(ball)
-    time.sleep(1)
+# while True:
+#     drawScreen(players,ball[0])
+#     updateBall(ball)
+#     time.sleep(1)
 
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+FPS = 60
+
+# FIXME: ta bort om vi bestämmer oss för att slopa terminalgränssnittet!
+def termpos2winpos(termpos):
+    x,y = termpos
+    return (x*WIN_WIDTH/30, y*WIN_HEIGHT/10)
+
+def draw(surface):
+    for p in players:
+        winpos = termpos2winpos(p)
+        r = pygame.Rect(winpos[0], winpos[1], 20, 80)
+        pygame.draw.rect(surface, (255,255,255), r)
+    ballpos = termpos2winpos(ball[0])
+    pygame.draw.circle(surface, (255,255,255), ballpos, 5)
+
+window = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    window.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    draw(window)
+    updateBall(ball)
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
